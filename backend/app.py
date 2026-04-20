8556525506c571dbdff090749d75a6db781713d4
shalakapoojari <shalakapoojari677@gmail.com>
2026-04-20 19:10:39 +0530

    changes

--- backend/app.py
+++ backend/app.py
@@ -18,7 +18,8 @@ from sqlalchemy import func, desc, and_, or_
 #  APP & CONFIG
 # ─────────────────────────────────────────────
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY")
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
